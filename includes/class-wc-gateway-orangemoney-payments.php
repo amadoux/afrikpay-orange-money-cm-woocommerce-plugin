f44d206d99f6239b55a9826123c1c19c7ee6ff28
afrikpaysas <58859378+afrikpaysas@users.noreply.github.com>
2020-04-24 11:08:41 +0100

    Update class-wc-gateway-orangemoney-payments.php

--- includes/class-wc-gateway-orangemoney-payments.php
+++ includes/class-wc-gateway-orangemoney-payments.php
@@ -13,11 +13,12 @@ class WC_Gateway_OM extends WC_Payment_Gateway {}
 <input type="hidden" name="amount" value="<?php echo $_GET["totalamount"] ?>" />
 <input type="hidden" name="phonenumber" value="" />
 <input type="hidden" name="purchaseref" value="<?php $json = json_decode($_GET["custom"], true); echo $json['order_id']; ?>" />
+<input type="hidden" name="notifurl" value="<?php echo $_GET["notify_url"]; ?>" />
 <input type="hidden" name="accepturl" value="<?php echo $_GET["return"]; ?>" />
 <input type="hidden" name="cancelurl" value="<?php echo $_GET["cancel_return"]; ?>" />
 <input type="hidden" name="declineurl" value="<?php echo $_GET["cancel_return"]; ?>" />
 <input type="hidden" name="text" value="<?php echo $_GET["text"]; ?>" />
-<input type="hidden" name="language" value="fr" /> 
+<input type="hidden" name="language" value="fr" />  
 
 </form>
 <script type="text/javascript">
